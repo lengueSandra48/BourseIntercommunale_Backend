932dd773ac1b948b8b75b6029816e617847dc71b
stacvirus <parfaitandre5@gmail.com>
2024-08-08 16:35:19 +0100

    project initialization

---
+++ src/main/java/com/pk48/BourseIntercommunale/BourseIntercommunaleApplication.java
@@ -0,0 +1,13 @@
+package com.pk48.BourseIntercommunale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BourseIntercommunaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BourseIntercommunaleApplication.class, args);
+	}
+
+}
